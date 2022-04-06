e8237fca1fd40c339ec1571fdf56e704507b4393
Victor Henrique Braz de Araújo <victorhda@ciandt.com>
2022-04-06 20:26:39 -0300

    criaçao classes Exception e seus construtores

---
+++ ByteBank/Exceptions/SaldoInsuficienteException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank.Exceptions
+{
+    public class SaldoInsuficienteException : OperacaoFinanceiraException
+    {
+        public double Saldo { get; }
+        public double ValorSaque { get; }
+
+        public SaldoInsuficienteException()
+        {
+
+        }
+        
+        public SaldoInsuficienteException(double saldo, double valorSaque) : this("O valor do Saque não pode ser maior que o Saldo. (Valor do saque: " + valorSaque + ". Saldo na Conta: " + saldo + ").")
+        {
+            Saldo = saldo;
+            ValorSaque = valorSaque;
+        }
+
+        public SaldoInsuficienteException(string mensagem) : base(mensagem)
+        {
+
+        }
+
+        public SaldoInsuficienteException(string mensagem, Exception excecaoInterna) : base(mensagem, excecaoInterna)
+        {
+
+        }
+    }
+}
