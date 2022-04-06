40949b5a8aeb19b0d54bdbf943628357efa80350
Victor Henrique Braz de Araújo <victorhda@ciandt.com>
2022-04-06 20:22:05 -0300

    Criação class Cliente

---
+++ ByteBank/Clientes/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank.Clientes
+{
+    public class Cliente
+    {
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public string Profissao { get; set; }
+    }
+}
