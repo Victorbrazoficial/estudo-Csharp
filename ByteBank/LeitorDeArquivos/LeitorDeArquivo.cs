1b2d7b5440fb6b9dc2988a4fd9c122cd747bad70
Victor Henrique Braz de Araújo <victorhda@ciandt.com>
2022-04-06 20:30:20 -0300

    Utilizando metodo Dispose da classe IDisposable

---
+++ ByteBank/LeitorDeArquivos/LeitorDeArquivo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ByteBank.LeitorDeArquivos
+{
+    internal class LeitorDeArquivo : IDisposable
+    {
+        public string Arquivo { get; }
+
+        public LeitorDeArquivo(string arquivo)
+        {
+            Arquivo = arquivo;
+
+            throw new FileNotFoundException();
+
+            Console.WriteLine("Abrindo Arquivo.");
+        }
+
+        public string LerProximaPagina()
+        {
+            Console.WriteLine("Lendo Linha.");
+
+            //throw new IOException();
+
+            return "Linha do arquivo";
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Fechando Arquivo.");
+        }
+    }
+}
